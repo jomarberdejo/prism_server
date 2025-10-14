9ece792f836aea3f4a81103d31fbaf65c6b556ed
jomarberdejo <jomar.berdejo1@gmail.com>
2025-10-14 16:14:13 +0800

    modified auth

--- src/controllers/auth/routes.ts
+++ src/controllers/auth/routes.ts
@@ -1,7 +1,5 @@
-import { Context, Hono } from "hono";
-import { getCurrentUser, login, logout, refresh, register } from "@/controllers/auth";
-import { JWT_SECRET } from "@/constants/cookies";
-import { verify } from "hono/jwt";
+import { Hono } from "hono";
+import { login, logout, refresh, register } from "@/controllers/auth";
 
 const router = new Hono();
 
@@ -9,21 +7,5 @@ router.post("/auth/register", register);
 router.post("/auth/login", login);
 router.post("/auth/logout", logout);
 router.post("/auth/refresh", refresh);
-router.get("/me", async (c: Context, next) => {
-  try {
-    const token = c.req.header("Authorization")?.replace("Bearer ", "");
-    console.log(token)
-    if (!token) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
-
-    const payload =  verify(token, JWT_SECRET, "HS256");
-    c.set("user", payload);
-    await next();
-  } catch {
-    return c.json({ error: "Invalid token" }, 401);
-  }
-}, getCurrentUser);
-
 
 export default router;

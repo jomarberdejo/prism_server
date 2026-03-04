bcbedbd70a7a7a606904f313e214e9066810aad4
jomarberdejo <berdejojomar@gmail.com>
2026-03-04 11:24:22 +0800

    modified docker config

---
+++ prisma/migrations/20260304031531/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `ppa` MODIFY `budgetAllocation` LONGTEXT NULL,
+    MODIFY `actualOutput` LONGTEXT NULL,
+    MODIFY `remarks` LONGTEXT NULL,
+    MODIFY `delayedReason` LONGTEXT NULL;

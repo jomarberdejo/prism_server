17947191c1862161ab383b38a7e5ea432c6c478e
jomarberdejo <berdejojomar@gmail.com>
2026-03-04 10:09:10 +0800

    updated migration

---
+++ prisma/migrations/20260304015518/migration.sql
@@ -0,0 +1,107 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `role` ENUM('SUPER_ADMIN', 'ADMIN', 'EDITOR', 'VIEWER') NOT NULL DEFAULT 'VIEWER',
+    `isDepartmentHead` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `pushToken` VARCHAR(191) NULL,
+    `status` ENUM('PENDING', 'ACTIVE', 'REJECTED') NOT NULL DEFAULT 'PENDING',
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
+    INDEX `User_email_idx`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(240) NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Session_token_key`(`token`),
+    INDEX `Session_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Sector` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ImplementingUnit` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `ImplementingUnit_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PPA` (
+    `id` VARCHAR(191) NOT NULL,
+    `task` VARCHAR(191) NOT NULL,
+    `description` LONGTEXT NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `venue` VARCHAR(191) NULL,
+    `startDate` DATETIME(3) NOT NULL,
+    `dueDate` DATETIME(3) NOT NULL,
+    `budgetAllocation` VARCHAR(191) NULL,
+    `approvedBudget` VARCHAR(191) NULL,
+    `expectedOutput` VARCHAR(191) NOT NULL,
+    `actualOutput` VARCHAR(191) NULL,
+    `remarks` VARCHAR(191) NULL,
+    `delayedReason` VARCHAR(191) NULL,
+    `sectorId` VARCHAR(191) NOT NULL,
+    `implementingUnitId` VARCHAR(191) NOT NULL,
+    `status` ENUM('NOT_STARTED', 'IN_PROGRESS', 'COMPLETED', 'DELAYED', 'ARCHIVED') NOT NULL DEFAULT 'NOT_STARTED',
+    `dayBeforeNotifiedAt` DATETIME(3) NULL,
+    `hourBeforeNotifiedAt` DATETIME(3) NULL,
+    `archivedAt` DATETIME(3) NULL,
+    `userId` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_PPAAttendees` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_PPAAttendees_AB_unique`(`A`, `B`),
+    INDEX `_PPAAttendees_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ImplementingUnit` ADD CONSTRAINT `ImplementingUnit_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PPA` ADD CONSTRAINT `PPA_sectorId_fkey` FOREIGN KEY (`sectorId`) REFERENCES `Sector`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PPA` ADD CONSTRAINT `PPA_implementingUnitId_fkey` FOREIGN KEY (`implementingUnitId`) REFERENCES `ImplementingUnit`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PPA` ADD CONSTRAINT `PPA_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_PPAAttendees` ADD CONSTRAINT `_PPAAttendees_A_fkey` FOREIGN KEY (`A`) REFERENCES `PPA`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_PPAAttendees` ADD CONSTRAINT `_PPAAttendees_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;

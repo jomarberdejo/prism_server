5839ed03f962b16f166022a6a75c703b36dc82ca
jomarberdejo <berdejojomar@gmail.com>
2026-03-05 16:02:43 +0800

    removed duplication of notif

--- src/controllers/auth/routes.ts
+++ src/controllers/auth/routes.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import { authHandler } from "@/controllers/auth";
+import { authMiddleware } from "@/middlewares/auth";
 const router = new Hono();
 
 router.post("/auth/register", authHandler.register);
 router.post("/auth/login", authHandler.login);
-router.post("/auth/logout", authHandler.logout);
+router.post("/auth/logout", authMiddleware, authHandler.logout);
 
 export default router;

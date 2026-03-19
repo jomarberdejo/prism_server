efb6011d2c91633158e75c60eccb41c7d82fd0f2
jomarberdejo <berdejojomar@gmail.com>
2026-03-19 13:41:15 +0800

    modified config deployment

--- src/middlewares/rbac.ts
+++ src/middlewares/rbac.ts
@@ -1,6 +1,5 @@
 import { ForbiddenError } from "@/utils/error";
 import type { Context, Next } from "hono";
-import { StatusCodes } from "http-status-codes";
 
 export const requireRole = (allowedRoles: string[]) => {
   return async (c: Context, next: Next) => {
